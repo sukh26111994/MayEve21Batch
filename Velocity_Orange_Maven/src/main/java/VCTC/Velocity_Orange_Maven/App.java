960f7f3c89e6d69c71260042759798da364017f6
sukh26111994 <90309548+sukh26111994@users.noreply.github.com>
2021-09-08 16:07:25 +0530

    changesConfirm

---
+++ Velocity_Orange_Maven/src/main/java/VCTC/Velocity_Orange_Maven/App.java
@@ -0,0 +1,13 @@
+package VCTC.Velocity_Orange_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
